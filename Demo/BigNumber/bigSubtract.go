396cf736a45aadff4273323425d47d7abf24a1bb
zhangjiangchuan <zhangjiangchuan@xiaomi.com>
2017-09-06 20:19:11 +0800

    2017.9.6

---
+++ Demo/BigNumber/bigSubtract.go
@@ -0,0 +1,11 @@
+/** 
+  * Author: Juntaran 
+  * Email:  Jacinthmail@gmail.com 
+  * Date:   2017/9/6 19:14 
+  */
+
+package BigNumber
+
+func BigSubtract(num1, num2 string) (string, error) {
+	return "", nil
+}
---
+++ Demo/BigNumber/main/main.go
@@ -0,0 +1,15 @@
+/** 
+  * Author: Juntaran 
+  * Email:  Jacinthmail@gmail.com 
+  * Date:   2017/9/6 17:30 
+  */
+
+package main
+
+import "Go_In_Action/Demo/BigNumber"
+
+func main() {
+	a := "+500000"
+	b := "50000"
+	BigNumber.BigAdd(a, b)
+}
