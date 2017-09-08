3e0fc72034e5d05417ae09200d5e5b4d25094414
Juntaran <jacinthmail@gmail.com>
2017-09-09 03:38:35 +0800

    2017.9.9

---
+++ Demo/BigNumber/bigAddReduce.go
@@ -0,0 +1,276 @@
+/** 
+  * Author: Juntaran 
+  * Email:  Jacinthmail@gmail.com 
+  * Date:   2017/9/6 17:01 
+  */
+
+package BigNumber
+
+import (
+	"math"
+	"strconv"
+	"errors"
+)
+
+// 大数相加
+func BigAdd(num1 string, num2 string) (string, error) {
+
+	// 正负号分类，只需要写基本的 bigAdd 和 bigReduce 即可
+	if num1[0] >= '0' && num1[0] <= '9' {
+		if num2[0] >= '0' && num2[0] <= '9' {
+			return bigAdd(num1, num2)
+		} else if num2[0] == '+' {
+			return bigAdd(num1, num2[1:])
+		} else if num2[0] == '-' {
+			return bigReduce(num1, num2[1:])
+		} else {
+			return "", errors.New("Error: Input")
+		}
+	} else if num1[0] == '+' {
+		if num2[0] >= '0' && num2[0] <= '9' {
+			return bigAdd(num1[1:], num2)
+		} else if num2[0] == '+' {
+			return bigAdd(num1[1:], num2[1:])
+		} else if num2[0] == '-' {
+			return bigReduce(num1[1:], num2[1:])
+		} else {
+			return "", errors.New("Error: Input")
+		}
+	} else if num1[0] == '-' {
+		if num2[0] >= '0' && num2[0] <= '9' {
+			return bigReduce(num2, num1[1:])
+		} else if num2[0] == '+' {
+			return bigReduce(num2[1:], num1[1:])
+		} else if num2[0] == '-' {
+			ret, err :=  bigAdd(num1[1:], num2[1:])
+			if err != nil {
+				return "", err
+			} else {
+				return "-" + ret, nil
+			}
+		} else {
+			return "", errors.New("Error: Input")
+		}
+	} else {
+		return "", errors.New("Error: Input")
+	}
+}
+
+// 大数相减
+func BigReduce(num1 string, num2 string) (string, error) {
+
+	// 正负号分类，只需要写基本的 bigAdd 和 bigReduce 即可
+	if num1[0] >= '0' && num1[0] <= '9' {
+		if num2[0] >= '0' && num2[0] <= '9' {
+			return bigReduce(num1, num2)
+		} else if num2[0] == '+' {
+			return bigReduce(num1, num2[1:])
+		} else if num2[0] == '-' {
+			return bigAdd(num1, num2[1:])
+		} else {
+			return "", errors.New("Error: Input")
+		}
+	} else if num1[0] == '+' {
+		if num2[0] >= '0' && num2[0] <= '9' {
+			return bigReduce(num1[1:], num2)
+		} else if num2[0] == '+' {
+			return bigReduce(num1[1:], num2[1:])
+		} else if num2[0] == '-' {
+			return bigAdd(num1[1:], num2[1:])
+		} else {
+			return "", errors.New("Error: Input")
+		}
+	} else if num1[0] == '-' {
+		if num2[0] >= '0' && num2[0] <= '9' {
+			ret, err :=  bigAdd(num1[1:], num2)
+			if err != nil {
+				return "", err
+			} else {
+				return "-" + ret, nil
+			}
+		} else if num2[0] == '+' {
+			ret, err :=  bigAdd(num1[1:], num2[1:])
+			if err != nil {
+				return "", err
+			} else {
+				return "-" + ret, nil
+			}
+		} else if num2[0] == '-' {
+			return bigReduce(num2[1:], num1[1:])
+		} else {
+			return "", errors.New("Error: Input")
+		}
+	} else {
+		return "", errors.New("Error: Input")
+	}
+}
+
+// 两个正整数相加
+func bigAdd(num1, num2 string) (string, error) {
+	len1 := len(num1)
+	len2 := len(num2)
+	lenMax := int(math.Max(float64(len1), float64(len2)))
+	var ret []uint8
+	var result string
+
+	var a = make([]uint8, lenMax + 1)
+	var b = make([]uint8, lenMax + 1)
+	var c = make([]uint8, lenMax + 1)
+
+	// 检验 num1 和 num2
+	var str1 = make([]uint8, lenMax + 1)
+	var str2 = make([]uint8, lenMax + 1)
+
+	for i := 0; i < len1; i++ {
+		str1[i] = num1[i]
+		if str1[i] > '9' || str1[i] < '0' {
+			return "", errors.New("Error: Input")
+		}
+	}
+	for j := 0; j < len2; j++ {
+		str2[j] = num2[j]
+		if str2[j] > '9' || str2[j] < '0' {
+			return "", errors.New("Error: Input")
+		}
+	}
+
+	for i := 0; i < lenMax; i++ {
+		if len1 - i - 1 < 0 {
+			b[len2-i-1] = str2[i] - '0'
+			continue
+		}
+		if len2 - i - 1 < 0 {
+			a[len1-i-1] = str1[i] - '0'
+			continue
+		}
+		a[len1-i-1] = str1[i] - '0'
+		b[len2-i-1] = str2[i] - '0'
+	}
+
+	for i := 0; i < lenMax; i++ {
+		c[i] = a[i] + b[i]
+	}
+
+	for i := 0; i < lenMax; i++ {
+		c[i+1] = c[i] / 10 + c[i+1]
+		c[i] = c[i] % 10
+	}
+	if c[lenMax] != 0 {
+		for i := lenMax; i >= 0; i-- {
+			ret = append(ret, c[i])
+		}
+	} else {
+		for i := lenMax - 1; i >= 0; i-- {
+			ret = append(ret, c[i])
+		}
+	}
+
+	for _, v := range ret {
+		result += strconv.Itoa(int(v))
+	}
+
+	return result, nil
+}
+
+// 两个正整数相减，num1 - num2  如果 num1 < num2 会先算 num2 - num1 再加负号
+func bigReduce(num1, num2 string) (string, error) {
+	len1 := len(num1)
+	len2 := len(num2)
+	lenMax := int(math.Max(float64(len1), float64(len2)))
+	var ret []uint8
+	var result string
+	
+	// 判断 num1 和 num2 大小
+	var judge int  = 0
+	if len1 > len2 {
+		// do nothing
+	} else if len1 < len2 {
+		ret, err := bigReduce(num2, num1)
+		if err != nil {
+			return "", err
+		} else {
+			return "-" + ret, err
+		}
+	} else {
+		// len1 == len2
+		for i := 0; i < len1; i++ {
+			// 从左向右逐位判断 num1 和 num2 谁大
+			if num1[i] == num2[i] {
+				continue
+			} else if num1[i] > num2[i] {
+				judge = 1
+				break
+			} else {
+				judge = -1
+				break
+			}
+		}
+		switch judge {
+		case 0:
+			return "0", nil
+		case 1:
+			// do nothing
+			break
+		case -1:
+			num1, num2 = num2, num1
+			result = "-"
+		}
+	}
+
+	var a = make([]uint8, lenMax)
+	var b = make([]uint8, lenMax)
+	var c = make([]uint8, lenMax)
+
+	// 检验 num1 和 num2
+	var str1 = make([]uint8, lenMax)
+	var str2 = make([]uint8, lenMax)
+
+	for i := 0; i < len1; i++ {
+		str1[i] = num1[i]
+		if str1[i] > '9' || str1[i] < '0' {
+			return "", errors.New("Error: Input")
+		}
+	}
+	for j := 0; j < len2; j++ {
+		str2[j] = num2[j]
+		if str2[j] > '9' || str2[j] < '0' {
+			return "", errors.New("Error: Input")
+		}
+	}
+
+	for i := 0; i < lenMax; i++ {
+		if len1 - i - 1 < 0 {
+			b[len2-i-1] = str2[i] - '0'
+			continue
+		}
+		if len2 - i - 1 < 0 {
+			a[len1-i-1] = str1[i] - '0'
+			continue
+		}
+		a[len1-i-1] = str1[i] - '0'
+		b[len2-i-1] = str2[i] - '0'
+	}
+
+	for i := 0; i < lenMax; i++ {
+		c[i] = a[i] - b[i]
+		if c[i] < 0 {
+			c[i] += 10
+			c[i+1] --
+		}
+	}
+	var mark int
+	for i := lenMax - 1; i >= 0; i-- {
+		if c[i] != 0 {
+			mark = i
+			break
+		}
+	}
+	for j := mark; j >= 0; j-- {
+		ret = append(ret, c[j])
+	}
+	for _, v := range ret {
+		result += strconv.Itoa(int(v))
+	}
+
+	return result, nil
+}
--- Demo/BigNumber/main/main.go
+++ Demo/BigNumber/main/main.go
@@ -6,10 +6,26 @@
 
 package main
 
-import "Go_In_Action/Demo/BigNumber"
+import (
+	"Go_In_Action/Demo/BigNumber"
+	"fmt"
+)
 
 func main() {
-	a := "+500000"
-	b := "50000"
-	BigNumber.BigAdd(a, b)
+	a := "550555"
+	b := "-551555"
+
+	c, err1 := BigNumber.BigAdd(a, b)
+	if err1 != nil {
+		fmt.Println(err1)
+	} else {
+		fmt.Println(c)
+	}
+
+	ret, err2 := BigNumber.BigReduce(c, b)
+	if err2 != nil {
+		fmt.Println(err2)
+	} else {
+		fmt.Println(ret)
+	}
 }
