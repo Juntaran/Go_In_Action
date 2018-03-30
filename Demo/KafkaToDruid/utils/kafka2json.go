10ec65f4cb633835c03dd19335facc1cdd14f497
Juntaran <jacinthmail@gmail.com>
2018-03-30 03:17:56 -0500

    2018.3.30

--- Demo/KafkaToDruid/utils/kafka2json.go
+++ Demo/KafkaToDruid/utils/kafka2json.go
@@ -27,7 +27,7 @@ func KafkaToJson(testStr string) {
 
 	// 正式代码
 	timestamp := NginxTimeConvert(s[5])
-	var ret = "{\"time\": " + timestamp + ", \"http_host\": \"" + s[0] + "\", \"server_addr\": \"" + s[1] + "\", \"hostname\": \"" + s[2] + "\", \"remote_addr\": \"" + s[3] + "\", \"http_x_forwarded_for\": \"" + s[4] + "\", \"time_local\": \"" + s[5] + "\", \"request_uri\": \"" + s[6] + "\", \"request_length\": " + s[7] + ", \"bytes_sent\": " + s[8] + ", \"request_time\": " + s[9] + ", \"status\": " + s[10] + ", \"upstream_addr\": \"" + s[11] + "\", \"upstream_response_time\": " + s[12] + "\", \"scheme\": \"" + s[13] + "\"}"
+	var ret = "{\"time\": \"" + timestamp + "\", \"http_host\": \"" + s[0] + "\", \"server_addr\": \"" + s[1] + "\", \"hostname\": \"" + s[2] + "\", \"remote_addr\": \"" + s[3] + "\", \"http_x_forwarded_for\": \"" + s[4] + "\", \"time_local\": \"" + s[5] + "\", \"request_uri\": \"" + s[6] + "\", \"request_length\": " + s[7] + ", \"bytes_sent\": " + s[8] + ", \"request_time\": " + s[9] + ", \"status\": " + s[10] + ", \"upstream_addr\": \"" + s[11] + "\", \"upstream_response_time\": " + s[12] + "\", \"scheme\": \"" + s[13] + "\"}"
 	//fmt.Println(ret)
 
 	// 测试无需转换格式
