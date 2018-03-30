2a658279838e221d66918d18f59bd9bd15f54209
Juntaran <jacinthmail@gmail.com>
2018-03-30 03:17:02 -0500

    2018.3.30

--- Demo/KafkaToDruid/utils/timeConvert.go
+++ Demo/KafkaToDruid/utils/timeConvert.go
@@ -14,5 +14,6 @@ import (
 // nginx 时间转化为时间戳
 func NginxTimeConvert(timeNg string) string {
 	tm, _ := time.Parse("02/Jan/2006:15:04:05 +0800", timeNg)
-	return strconv.Itoa(int(tm.Unix()) - 8*3600)
+	//return strconv.Itoa(int(tm.Unix()) - 8*3600)
+	return time.Unix(tm.Unix()-3600*16, 0).Format("2006-01-02T15:04:05Z")
 }
